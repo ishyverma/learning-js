4b3cc9c1b8c4f4fa4e5195efa0461dd88380d516
shyam verma <ishyamver@gmail.com>
2024-08-07 10:34:50 +0000

    add file for JavaScript datatypes

---
+++ 01-basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // Treat all js code as newer version
+
+console.log(3 + 3);
+console.log("Shyam");
+
+let fname = "Shyam";
+let age = 17; 
+let isLoggedIn = true;
+let state = null;
+let address; // Returns undefined. 
+
+// Number, bigInt, string, boolean, null, undefined, symbol, object, array
+
+console.log(typeof fname);
+console.log(typeof age);
+console.log(typeof undefined); // Returns undefined
+console.log(typeof null); // Returns object. 
+
+
+   
