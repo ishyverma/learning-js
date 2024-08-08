f638d67085c23cb4ef39c14edf0123c04d68a43f
shyam verma <ishyamver@gmail.com>
2024-08-08 04:40:16 +0000

    added a strings methods file

---
+++ 01-basics/strings.js
@@ -0,0 +1,8 @@
+const fname = "shyam";
+const repoCount = 50;
+
+console.log(`Hello my name is ${fname} and my repocount is ${repoCount}`);
+
+const gameName = new String('shyam verma'); // Another way of declaring a string. But String is a object here.
+console.log(gameName[0]); 
+console.log(gameName.__proto__); 
