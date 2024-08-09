3971ed83c3e34cbd4d38923428e0189d67fa9de0
shyam verma <ishyamver@gmail.com>
2024-08-09 05:30:20 +0000

    more about scopes

--- 03-basics/scopes.js
+++ 03-basics/scopes.js
@@ -1,9 +1,38 @@
-if (true) {
-    let a = 10;
-    const b = 20;
-    var c = 30;
+// if (true) {
+//     let a = 10;
+//     const b = 20;
+//     var c = 30;
+// }
+
+// console.log(a); // Error.
+// console.log(b); // Error.
+// console.log(c); // Will print 30 because var creates a variable in global scope.  
+
+
+
+// function one() {
+//     const username = "shyam";
+
+//     function two() {
+//         const website = "youtube";
+//         console.log(username); 
+//     }
+//     two()
+//     console.log(website);
+
+    
+// }
+// one();
+
+addOne(5)
+function addOne(num) {
+    return num++;
+
 }
 
-console.log(a); // Error.
-console.log(b); // Error.
-console.log(c); // Will print 30 because var creates a variable in global scope. 
+
+
+addTwo(5);
+const addTwo = function(num) {
+    return num + 2;
+}
