059f5a08d70f60075d8b82910aafae3f9ac143b1
ishyverma <ishyamver@gmail.com>
2024-08-10 09:32:53 +0530

    control flow

---
+++ 04-controlFlow/one.js
@@ -0,0 +1,8 @@
+// If
+
+if (true) {
+    // Will execute the code in this scope. 
+}
+if (false) {
+    // Will not execute the code in this scope.
+}
---
+++ 04-controlFlow/truthyFalsy.js
@@ -0,0 +1,50 @@
+const userEmail = "s@gmail.com";
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Don't have user email");
+}
+
+/* 
+
+false => falsy value
+0 => falsy value
+-0 => falsy value
+0n/BigInt(0) => falsy value
+null => falsy value
+undefined => falsy value
+NaN => false value
+"" => falsy value
+
+*/
+
+
+/*
+
+"0" => truthy value
+"false" => truthy value
+" " => truthy value
+[] => truthy value
+{} => truthy value
+function() {} => truthy value
+
+
+*/
+
+
+// Nullish Coalescing Operator (??): null, undefined
+let val1;
+// val1 = 5 ?? 10;
+// val1 = null ?? 10;
+// val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+console.log(val1);
+
+
+// Terniary Operator
+
+// condition ? true : false;
+
+const iceTeaPrice = 100;
+iceTeaPrice >= 80 ? console.log("Less than 80"): console.log("More than 80");
