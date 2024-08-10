5b837e0f085cc82e3fff0a9a6edffcf80aa01810
ishyverma <ishyamver@gmail.com>
2024-08-10 11:09:56 +0530

    while and do-while loop

---
+++ 05-loops/two.js
@@ -0,0 +1,19 @@
+let i = 0;
+while (i <= 10) {
+    console.log(i);
+    i += 2;
+}
+
+
+let myArray = ["flash", "batman", "superman"];
+let j = 0
+while(j<myArray.length) {
+    console.log(myArray[j]);
+    j++;
+}
+
+let score = 1;
+do{
+    console.log(`Score is: ${score}`);
+    score++;
+} while (score <= 10)
